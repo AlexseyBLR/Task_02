7bcef9b16353469e868c59155d7b305b22bd5037
AlexeyMeleschenya <meleschenya1997@gmail.com>
2018-01-31 15:09:22 +0300

    2

--- task/src/main/java/dao/property/Properties.java
+++ task/src/main/java/dao/property/Properties.java
@@ -1,8 +1,10 @@
 package dao.property;
 
+import java.io.File;
+
 public class Properties {
 
-    public static final String path = "C:\\Users\\Lesha\\Desktop\\task\\src\\main\\resources\\goods_DB.txt";
+    public static final String path =  "src" + File.separator + "main" + File.separator + "resources" + File.separator + "goods_DB.txt";
 
     public static final int titlePosition = 9;
     public static final int pricePosition = 7;
